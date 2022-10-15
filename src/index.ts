c839078f07c73ecb530d437390dc25b1d6a9ebb5
Jayson De los Reyes <jaysondelosreyes@Jaysons-MacBook-Pro.local>
2022-10-15 23:28:05 +0800

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
+import * as morgan from "morgan";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((_, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.status(200).send("Hello, World!");
+const app = express();
+app.use(cors({ origin: true }));
+app.use(morgan("common"));
+
+app.get("/ping", (_, res) => {
+  res.json({ message: "pong!" });
 });
+
+app.get("/users/info", (_, res) => {
+  res.json({ name: "Jayson" });
+});
+
+export const api = functions.https.onRequest(app);
